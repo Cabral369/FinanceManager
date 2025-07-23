94d2fab0806713e64e501582c1984157799e9bdb
Cabral369 <nicolas.cabral.carvalho@gmail.com>
2025-07-22 21:18:15 -0300

    teste

--- financeManager.Domain/Entities/User.cs
+++ financeManager.Domain/Entities/User.cs
@@ -5,4 +5,6 @@ public class User : BaseEntity, IAutoGenerate
     public string Name { get; set; } = string.Empty;
     public string Surname { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+
+    public int MyProperty { get; set; }
 }
